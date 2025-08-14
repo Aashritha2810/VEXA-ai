19cbc52b74416127e486a366524f6bf0970f0408
Aashritha2810 <aashritha.tiru@gmail.com>
2025-08-14 13:57:33 +0530

    imported SpeechRecognition and PyAudio libraries

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -10,4 +11,23 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-speak("I am NOVA")
+def takeCommand():
+    r= sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source, 10, 6)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-US')
+        print(f"User said: {query}")
+    except Exception as e:
+        print("Sorry, I didn't catch that.")
+        return None
+    return query.lower()
+
+
+text= takeCommand()
+
+speak(text)
