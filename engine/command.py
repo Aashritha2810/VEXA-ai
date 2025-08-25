08059357ffc91cfe6888f453d5113e1b1886a834
Aashritha2810 <aashritha.tiru@gmail.com>
2025-08-25 16:59:30 +0530

    connected command.py to frontend using eel library

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import speech_recognition as sr
+import eel 
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -11,23 +12,25 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+@eel.expose 
 def takeCommand():
     r= sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
+        eel.DisplayMessage("Listening...")
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source, 10, 6)
     try:
         print("Recognizing...")
+        eel.DisplayMessage("Recognizing...")
         query = r.recognize_google(audio, language='en-US')
         print(f"User said: {query}")
+        eel.DisplayMessage(query)
+        speak(query)
+        eel.ShowHood()
     except Exception as e:
         print("Sorry, I didn't catch that.")
         return None
     return query.lower()
 
-
-text= takeCommand()
-
-speak(text)
